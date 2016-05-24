f030fb1a6ce82ad0ef5880f94d2cb5c3631030e1
pmbennett <paul.bennett@pure360.com>
2016-05-24 14:08:34 +0100

    Creating control messages component

--- app/components/add-bet-form/add-bet-form.component.js
+++ app/components/add-bet-form/add-bet-form.component.js
@@ -5,11 +5,13 @@ import { ValidationService } from '../../services/validation.service';
 import { BetStoreService } from '../../services/bet-store.service';
 import template from './add-bet-form.template.html';
 import { BetModel } from  '../../models/bet.model';
+//import { ControlMessages } from '../common/control-messages.component';
 import { BET_TYPES, BET_EVENTS, BOOKMAKERS, EXCHANGES } from  '../../constants/constants';
 
 @Component({
   selector: 'add-bet-form',
-  template: template
+  template: template,
+  //directives: [ControlMessages]
 })
 export class AddBetFormComponent {
   constructor(betStore: BetStoreService, formBuilder: FormBuilder) {
@@ -23,20 +25,34 @@ export class AddBetFormComponent {
 
     this.bet = new BetModel();
 
+    this.buildForm();
+  }
+
+  buildForm() {
     this.formControlGroup = this._formBuilder.group({
       bookmaker: [this.bet.bookmaker, Validators.required],
       exchange: [this.bet.exchange, Validators.required],
-      eventDate: [this.bet.eventDate, Validators.compose([Validators.required, ValidationService.dateValidator])],
+      eventDate: [this.bet.eventDate, Validators.compose([
+        Validators.required, ValidationService.dateValidator
+      ])],
       type: [this.bet.type, Validators.required],
       event: [this.bet.event, Validators.required],
-      value: [this.bet.value, Validators.compose([Validators.required, ValidationService.currencyValidator])],
+      value: [this.bet.value, Validators.compose([
+        Validators.required, ValidationService.currencyValidator
+      ])],
     });
+
+    this.buildingForm = false;
   }
 
   onSubmit() {
-    console.log('ADD', this.bet)
     this._betStore.add(this.bet);
 
     this.bet = new BetModel();
+
+    // There is no way to reset a form built with FormBuilder
+    // https://github.com/angular/angular/issues/4933
+    // So need to re-build form to reset everything
+    this.buildForm();
   }
 }
